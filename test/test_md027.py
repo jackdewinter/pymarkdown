cb711c7484267235a12918a54e3189235c7429f2
Jack De Winter <jack.de.winter@outlook.com>
2021-12-12 13:21:27 -0800

    https://github.com/jackdewinter/pymarkdown/issues/97: formatting

--- test/test_md027.py
+++ test/test_md027.py
@@ -1699,6 +1699,7 @@ def test_md027_good_block_quote_ordered_list_html_block():
         expected_output, expected_error, expected_return_code
     )
 
+
 @pytest.mark.rules
 def test_md027_good_block_quote_ordered_list_html_block_with_indent():
     """
@@ -1730,6 +1731,7 @@ def test_md027_good_block_quote_ordered_list_html_block_with_indent():
         expected_output, expected_error, expected_return_code
     )
 
+
 @pytest.mark.rules
 def test_md027_good_block_quote_ordered_list_html_block_with_multiline():
     """
@@ -1757,6 +1759,7 @@ def test_md027_good_block_quote_ordered_list_html_block_with_multiline():
     execute_results.assert_results(
         expected_output, expected_error, expected_return_code
     )
+
 
 @pytest.mark.rules
 @pytest.mark.skip
