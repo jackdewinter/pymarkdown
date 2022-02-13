f454f97aaef411b6caf37ec27925cb86b2bed5da
Jack De Winter <jack.de.winter@outlook.com>
2022-02-13 08:43:22 -0800

    https://github.com/jackdewinter/pymarkdown/issues/154 - Cleaning up.

--- pymarkdown/plugins/rule_md_003.py
+++ pymarkdown/plugins/rule_md_003.py
@@ -81,6 +81,47 @@ class RuleMd003(RulePlugin):
         if self.__style_type != RuleMd003.__consistent_style:
             self.__actual_style_type = self.__style_type
 
+    def __handle_simple_styles(self, heading_style_type, is_heading_level_1_or_2):
+        is_heading_bad = bool(heading_style_type != self.__actual_style_type)
+
+        if (
+            is_heading_bad
+            and self.__allow_consistent_setext_update
+            and self.__actual_style_type == RuleMd003.__setext_style
+            and heading_style_type == RuleMd003.__atx_style
+            and not is_heading_level_1_or_2
+        ):
+
+            is_heading_bad = False
+            self.__actual_style_type = RuleMd003.__setext_with_atx_style
+
+        expected_style_type = self.__actual_style_type
+        return is_heading_bad, expected_style_type
+
+    def __handle_complex_styles(self, heading_style_type, is_heading_level_1_or_2):
+
+        is_heading_bad, expected_style_type = False, None
+        if self.__actual_style_type == RuleMd003.__setext_with_atx_style:
+            base_atx_style = RuleMd003.__atx_style
+        else:
+            assert self.__actual_style_type == RuleMd003.__setext_with_atx_closed_style
+            base_atx_style = RuleMd003.__atx_closed_style
+        if not (
+            (is_heading_level_1_or_2 and heading_style_type == RuleMd003.__setext_style)
+            or (not is_heading_level_1_or_2 and heading_style_type == base_atx_style)
+        ):
+            is_heading_bad = True
+            expected_style_type = (
+                RuleMd003.__setext_style if is_heading_level_1_or_2 else base_atx_style
+            )
+            if (
+                expected_style_type == RuleMd003.__setext_style
+                and is_heading_level_1_or_2
+                and self.__actual_style_type == RuleMd003.__setext_with_atx_style
+            ):
+                expected_style_type = RuleMd003.__setext_with_atx_style
+        return is_heading_bad, expected_style_type
+
     def next_token(self, context, token):
         """
         Event that a new token is being processed.
@@ -95,52 +136,13 @@ class RuleMd003(RulePlugin):
             if not self.__actual_style_type:
                 self.__actual_style_type = heading_style_type
             elif self.__actual_style_type in RuleMd003.__simple_styles:
-                is_heading_bad = bool(heading_style_type != self.__actual_style_type)
-
-                if (
-                    is_heading_bad
-                    and self.__allow_consistent_setext_update
-                    and self.__actual_style_type == RuleMd003.__setext_style
-                    and heading_style_type == RuleMd003.__atx_style
-                    and not is_heading_level_1_or_2
-                ):
-
-                    is_heading_bad = False
-                    self.__actual_style_type = RuleMd003.__setext_with_atx_style
-
-                expected_style_type = self.__actual_style_type
+                is_heading_bad, expected_style_type = self.__handle_simple_styles(
+                    heading_style_type, is_heading_level_1_or_2
+                )
             else:
-                if self.__actual_style_type == RuleMd003.__setext_with_atx_style:
-                    base_atx_style = RuleMd003.__atx_style
-                else:
-                    assert (
-                        self.__actual_style_type
-                        == RuleMd003.__setext_with_atx_closed_style
-                    )
-                    base_atx_style = RuleMd003.__atx_closed_style
-                if not (
-                    (
-                        is_heading_level_1_or_2
-                        and heading_style_type == RuleMd003.__setext_style
-                    )
-                    or (
-                        not is_heading_level_1_or_2
-                        and heading_style_type == base_atx_style
-                    )
-                ):
-                    is_heading_bad = True
-                    expected_style_type = (
-                        RuleMd003.__setext_style
-                        if is_heading_level_1_or_2
-                        else base_atx_style
-                    )
-                    if (
-                        expected_style_type == RuleMd003.__setext_style
-                        and is_heading_level_1_or_2
-                        and self.__actual_style_type
-                        == RuleMd003.__setext_with_atx_style
-                    ):
-                        expected_style_type = RuleMd003.__setext_with_atx_style
+                is_heading_bad, expected_style_type = self.__handle_complex_styles(
+                    heading_style_type, is_heading_level_1_or_2
+                )
 
             if is_heading_bad:
                 extra_data = (
