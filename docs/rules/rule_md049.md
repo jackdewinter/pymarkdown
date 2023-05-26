75b0519f5f3f89ae0a64edb0b0f50a56f73ae7c8
Michael Graf <michael.graf@digitalfrontiers.de>
2023-05-26 14:03:06 +0200

    add new rule to validate links

---
+++ docs/rules/rule_md049.md
@@ -0,0 +1,21 @@
+# Rule - MD049
+
+| Aliases        |
+|----------------|
+| `md049`        |
+| validate-refs  |
+
+## Summary
+
+Ensure that all references to parts of the documentation are valid.
+
+## Reasoning
+### Ensuring Correctness
+
+Invalid URLs for links and images can be a significant issue in documentation. 
+Manually validating each link and image is a time-consuming task. 
+With this rule, you can automatically validate the references to various parts of the documentation,
+such as links and images. 
+
+This helps to ensure the correctness and integrity of the documentation by identifying and flagging any invalid or broken references.
+
---
+++ test/rules/test_md049.py
@@ -0,0 +1,134 @@
+"""
+Module to provide tests related to the MD049 rule.
+"""
+import os
+
+import pytest
+
+from test.markdown_scanner import MarkdownScanner
+
+
+@pytest.mark.rules
+def test_md049_relative_file_invalid():
+    """
+    Test to verify that an error is thrown when a local relative URI doesn't
+    resolve to a file.
+    """
+
+    # Arrange
+    scanner = MarkdownScanner()
+    markdown_path = os.path.join(
+        "test", "resources", "rules", "md049", "file-relative-invalid.md"
+    )
+    supplied_arguments = [
+        "--strict-config",
+        "-e md049",
+        "scan",
+        markdown_path,
+    ]
+
+    expected_return_code = 1
+    expected_output = f"{markdown_path}:1:1: MD049: Local URIs should be valid (validate-refs)"
+    expected_error = ""
+
+    # Act
+    execute_results = scanner.invoke_main(arguments=supplied_arguments)
+
+    # Assert
+    execute_results.assert_results(
+        expected_output, expected_error, expected_return_code
+    )
+
+
+@pytest.mark.rules
+def test_md049_relative_file_valid():
+    """
+    Test to verify that no error is thrown when a local relative URI resolves
+    to a file.
+    """
+
+    # Arrange
+    scanner = MarkdownScanner()
+    markdown_path = os.path.join(
+        "test", "resources", "rules", "md049", "file-relative-valid.md"
+    )
+    supplied_arguments = [
+        "--strict-config",
+        "-e md049",
+        "scan",
+        markdown_path,
+    ]
+
+    expected_return_code = 0
+    expected_output = ""
+    expected_error = ""
+
+    # Act
+    execute_results = scanner.invoke_main(arguments=supplied_arguments)
+
+    # Assert
+    execute_results.assert_results(
+        expected_output, expected_error, expected_return_code
+    )
+
+
+@pytest.mark.rules
+def test_md049_absolute_file_invalid():
+    """
+    Test to verify that an error is thrown when a local absolute URI doesn't
+    resolve to a file.
+    """
+    # Arrange
+    scanner = MarkdownScanner()
+    markdown_path = os.path.join(
+        "test", "resources", "rules", "md049", "file-absolute-invalid.md"
+    )
+    supplied_arguments = [
+        "--strict-config",
+        "-e md049",
+        "scan",
+        markdown_path,
+    ]
+
+    expected_return_code = 1
+    expected_output = f"{markdown_path}:1:1: MD049: Local URIs should be valid (validate-refs)"
+    expected_error = ""
+
+    # Act
+    execute_results = scanner.invoke_main(arguments=supplied_arguments)
+
+    # Assert
+    execute_results.assert_results(
+        expected_output, expected_error, expected_return_code
+    )
+
+
+@pytest.mark.rules
+def test_md049_absolute_file_valid():
+    """
+    Test to verify that no error is thrown when a local absolute URI resolves to a file.
+    """
+
+    # Arrange
+    scanner = MarkdownScanner()
+    markdown_path = os.path.join(
+        "test", "resources", "rules", "md049", "file-absolute-valid.md"
+    )
+    supplied_arguments = [
+        "--strict-config",
+        "-e md049",
+        "scan",
+        markdown_path,
+    ]
+
+    expected_return_code = 0
+    expected_output = ""
+    expected_error = ""
+
+    # Act
+    execute_results = scanner.invoke_main(arguments=supplied_arguments)
+
+    # Assert
+    execute_results.assert_results(
+        expected_output, expected_error, expected_return_code
+    )
